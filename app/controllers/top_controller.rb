5b30605d5a2d8d9d6ad3b7d9362f3526259c800f
Yuta Takahashi <m1205123ing@gmail.com>
2018-01-10 09:24:23 +0000

    scrape test

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,40 @@
 class TopController < ApplicationController
+  require 'open-uri'
+  require 'nokogiri'
+  require 'mechanize'
+
   def index
+    # スクレイピング先のURL
+    url = 'http://www.yahoo.co.jp/'
+    html, charset = open_html(url)
+    # htmlをパース(解析)してオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    # タイトルを表示
+    @title = doc.title
   end
 
   def show
+    agent = Mechanize.new
+    page = agent.get('http://maxdelivery.com')
+
+    form = page.form('SearchForm')
+    form.searchString = "ham"
+    page = agent.submit(form)
+
+    @items = []
+    page.search("div.search-item").each_with_index do |item, i|
+      @items << item.attr('data-name')
+    end
+  end
+
+  private
+
+  def open_html(url)
+    charset = nil
+    html = open(url) do |file|
+      charset = file.charset # 文字種別を取得
+      file.read # htmlを読み込んで変数htmlに渡す
+    end
+    return html, charset
   end
 end
