a22a098f595677b180b25c6bf7efbc86e764adc3
Yuta Takahashi <m1205123ing@gmail.com>
2018-01-18 10:02:26 +0000

    install vue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
     config.webpacker.check_yarn_integrity = true  # Settings specified here will take precedence over those in config/application.rb.
-
+    config.x.webpacker[:dev_server_host] = "http://localhost:8080"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
